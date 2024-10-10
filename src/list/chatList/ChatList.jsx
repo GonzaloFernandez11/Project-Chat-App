f1916b7d35fd5cf1d0a749620e85a50f4f3ad73f
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-10-10 18:37:20 -0300

    first part chatlist ready

--- src/list/chatList/ChatList.jsx
+++ src/list/chatList/ChatList.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import './chatList.css'
 
 const ChatList = () => {
+    const [addMode, setAddMode] = useState(false);
+
     return (
         <div className="chatList">
             <div className="search">
@@ -8,7 +11,7 @@ const ChatList = () => {
                     <img src="/search.png" alt="" />
                     <input type="text" placeholder='Search' />
                 </div>
-                <img src="/plus.png" alt="" className='add' />
+                <img src={addMode ? "./minus.png" : "/plus.png"} alt="" className='add' onClick={() => setAddMode(prev => !prev)}/>
             </div>
         </div>
     )
