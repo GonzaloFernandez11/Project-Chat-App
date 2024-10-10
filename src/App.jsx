ae1168a4d22ccba8cbfd87bc27a2d1b7011e0374
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-10-10 14:56:03 -0300

    styles component preparation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Chat from './chat/Chat'
+import List from './list/List'
+import Detail from './detail/Detail'
+
 const App = ()  => {
   return (
     <div className="container">
-      
+      <List/>
+      <Chat/> 
+      <Detail/>
     </div>
   )
 }
