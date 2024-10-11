b2189e3cc19dd0931b622af42d48feea091fea79
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-10-11 17:21:31 -0300

    zustand install - config app - store creating

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import List from './components/lists/List';
 import Detail from './components/detail/Detail';
 import Login from './components/login/Login';
 import Notification from './components/notification/Notification';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './lib/firebase';
 
 const App = ()  => {
 const user = false;
+
+useEffect(() => {
+  const unSub = onAuthStateChanged(auth, (user) => {
+    console.log(user.uid);
+  });
+
+  return () => {
+    unSub();
+  };
+}, []);
 
   return (
     <div className="container">
