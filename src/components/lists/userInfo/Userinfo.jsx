ac5ad9ae6459a80da2010259677bc2255dbbbf0c
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-10-15 16:30:43 -0300

    fixes - detail info- chats info

--- src/components/lists/userInfo/Userinfo.jsx
+++ src/components/lists/userInfo/Userinfo.jsx
@@ -1,11 +1,15 @@
-import './userInfo.css'
+import { useUserStore } from '../../../lib/userStore';
+import './userInfo.css';
+
 
 const Userinfo = () => {
+
+  const { currentUser } = useUserStore();
     return (
         <div className="userInfo">
             <div className="user">
-                <img src="./avatar.png" alt="" />
-                <h3>Ricky Fort</h3>
+                <img src={ currentUser.avatar || "./avatar.png" } alt="" />
+                <h3>{currentUser.username}</h3>
             </div>
             <div className="icons">
                 <img src="./more.png" alt="" />
