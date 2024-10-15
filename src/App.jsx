e2015fce236f3384d1eed0dedafb02f94ef66540
Gonzalo Fernandez <ezekyel34@gmail.com>
2024-10-15 15:33:37 -0300

    loading - userstore - fetch info

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,38 @@ import Notification from './components/notification/Notification';
 import { useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './lib/firebase';
+import { useUserStore } from './lib/userStore';
 
 const App = ()  => {
-const user = false;
 
-useEffect(() => {
-  const unSub = onAuthStateChanged(auth, (user) => {
-    console.log(user.uid);
-  });
+  const {currentUser, isLoading, fetchUserInfo} = useUserStore();
 
-  return () => {
-    unSub();
-  };
-}, []);
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user.uid);
+    });
 
-  return (
-    <div className="container">
-      {user ? (
-        <>
-        <List/>
-        <Chat/> 
-        <Detail/>
-        </>
-      ) : (<Login />)}
-      <Notification/>
-    </div>
-  )
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  console.log(currentUser);
+
+  if(isLoading) return <div className='loading'>Loading...</div>;
+
+    return (
+      <div className="container">
+        {currentUser ? (
+          <>
+          <List/>
+          <Chat/> 
+          <Detail/>
+          </>
+        ) : (<Login />)}
+        <Notification/>
+      </div>
+    )
 }
 
 export default App
